d836c2756c9832d365d4444901d2d69d3082e2ad
mrivero105-oss <mrivero105@gmail.com>
2025-12-02 16:52:01 -0400

    Fix: Update Service Worker to v218 and force auto-reload on update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'american-pos-v217';
+const CACHE_NAME = 'american-pos-v218';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -19,6 +19,22 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => {
+            return self.clients.claim(); // Take control of all clients immediately
         })
     );
 });
